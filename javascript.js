ff15fc0f3f870af0cb999d0fadf07217c7d26e42
Casey Brock <casey.brock613@gmail.com>
2022-07-25 23:39:43 -0400

    Add Grid and Reset Button. -add initial grid of 16 squares -add mouseover event to color squares -add reset button to change number of squares

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
-console.log(test);
+createCanvas(16);
+promptForSquares();
+function createCanvas(userInput) {
+    let canvasDiv;
+    let numOfSquares = userInput;
+    let container = document.querySelector(".container");
+    container.setAttribute("style", `grid-template: repeat(${userInput}, 1fr) / repeat(${userInput}, 1fr)`);
+    for( i = 0; i < (numOfSquares ** 2); i++ ) {
+        canvasDiv = document.createElement("div");
+        canvasDiv.className = "canvas";
+        container.appendChild(canvasDiv);
+    }
+    onHover();
+}
+
+function onHover() {
+    let canvas = document.querySelectorAll(".canvas");
+    canvas.forEach(element => {
+        element.addEventListener("mouseover" , () => {
+        element.style.backgroundColor = "rgb(0, 0, 0)";
+        });
+    });
+}
+
+function promptForSquares() {
+     let squaresButton = document.querySelector(".num-of-squares");
+     squaresButton = document.addEventListener("click" , () => {
+        let squares = Number(prompt("Enter the number squares you'd like in each row, between 1 and 100."));
+        if(squares > 100 || squares <= 0|| isNaN(squares)) {
+            alert("That didnt work. Please try again")
+        } else {
+            clearCanvas();
+            createCanvas(squares);
+        }
+        
+     });
+}
+
+function clearCanvas() {
+    let canvas = document.querySelectorAll(".canvas");
+    canvas.forEach(element => element.remove());
+}
