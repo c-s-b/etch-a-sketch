3a9caf1d695f70c6339eafd32feaf25459b70681
Casey Brock <casey.brock613@gmail.com>
2022-07-28 23:26:05 -0400

    Add UI Friendly Input For Grid Size and Stop Color Options From Erasing Canvas

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
-createCanvas(25);
-function createCanvas(userInput) {
+createCanvas();
+function createCanvas(userInput = 25) {
     let canvasDiv;
     let numOfSquares = userInput;
     let container = document.querySelector(".container");
-    let squaresButton = document.querySelector(".num-of-squares");
+    let resetButton = document.querySelector(".reset");
     container.setAttribute("style", `grid-template: repeat(${userInput}, 1fr) / repeat(${userInput}, 1fr)`);
     for( i = 0; i < (numOfSquares ** 2); i++ ) {
         canvasDiv = document.createElement("div");
         canvasDiv.className = "canvas";
+        canvasDiv.style.backgroundColor = "rgb(255, 255, 255)"
         container.appendChild(canvasDiv);
     }
     onHover();
-    squaresButton.addEventListener("click" , promptForSquares);
+    resetButton.addEventListener("click" , clearCanvas);
+    promptForSquares();
 }
 
 function onHover() {
@@ -21,50 +23,57 @@ function onHover() {
     let tint = document.querySelector("#tint");
     if(blackRadio.checked === true) {
         canvas.forEach(element => {
-            element.addEventListener("mouseover" , () => {
-            element.style.backgroundColor = "rgb(0, 0, 0)";
+                element.addEventListener("mouseover" , () => {
+                    element.style.backgroundColor = "rgb(0, 0, 0)";  
+                });
             });
-        });
     } else if(colorRadio.checked === true) {
         canvas.forEach(element => {
             let red = Math.floor(Math.random() * 256);
             let blue = Math.floor(Math.random() * 256);
             let green = Math.floor(Math.random() * 256);
             element.addEventListener("mouseover" , () => {
-            element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+                element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;   
             });
         });
     } else if(tint.checked === true) {
         canvas.forEach(element => {
-            let tint = 1;
+            let tintAmount = 1;
             element.addEventListener("mouseover", () => {
-                if(tint <= 10){
-                element.style.backgroundColor = `rgba(0, 0, 0, ${(tint/10)})`;
-                tint++
+                element.style.backgroundColor = `rgba(0, 0, 0, ${(tintAmount/10)})`;
+                if(tintAmount <= 10) {
+                    tintAmount++
                 }
-            });
+            });      
         });
     }
     
-    blackRadio.addEventListener("click", promptForSquares);
+    blackRadio.addEventListener("click", onHover);
 
-    colorRadio.addEventListener("click", promptForSquares);
+    colorRadio.addEventListener("click", onHover);
 
-    tint.addEventListener("click", promptForSquares);
+    tint.addEventListener("click", onHover);
 
 }
 
+
 function promptForSquares() {    
-        let squares = Number(prompt("Enter the number squares you'd like in each row, between 1 and 100. The default is 25."));
-        if(squares > 100 || squares <= 0|| isNaN(squares)) {
-            alert("That didnt work. Please try again")
-        } else {
-            clearCanvas();
-            createCanvas(squares);
-        }
+        let squaresInput = document.querySelector(".squares");
+        let submitSquares = document.querySelector("#submit-squares");
+        submitSquares.addEventListener("click" , () => {
+            let squares = Number(squaresInput.value);
+            if(squares > 100 || squares <= 0|| isNaN(squares)) {
+                alert("That didnt work. Please try again")
+            } else {
+                clearCanvas();
+            }
+        });
     }
 
 function clearCanvas() {
     let canvas = document.querySelectorAll(".canvas");
+    let squaresInput = document.querySelector(".squares");
+    let squares = Number(squaresInput.value);
     canvas.forEach(element => element.remove());
+    createCanvas(squares);
 }
